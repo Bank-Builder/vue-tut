8060c2c64b6f315ea6b3ddad4eca9d81db4fd50b
Bank-Builder <andrew@turpin.co.za>
2020-04-09 14:21:14 +0200

    attribute binding added

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var app = new Vue({
     el: '#app',
     data: {
-        product: 'Socks'
+        product: 'Socks',
+        image: './images/vmSocks-green-onWhite.jpg',
+        home: './index.html'
     }
 })
